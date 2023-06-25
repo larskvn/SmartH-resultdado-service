e8be96e100d8d36aca2a45fb4abf8d4bc9951f66
lars kvn <kvnyarbis@gmail.com>
2023-06-24 19:56:07 -0500

    update

--- src/main/java/com/smartheal/resultadoservice/Controller/ResultadoController.java
+++ src/main/java/com/smartheal/resultadoservice/Controller/ResultadoController.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/resultado")
-@CrossOrigin("*")
 public class ResultadoController {
 
     @Autowired
---
+++ src/main/java/com/smartheal/resultadoservice/WebConfiguration/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.smartheal.resultadoservice.WebConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://smartheal.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
