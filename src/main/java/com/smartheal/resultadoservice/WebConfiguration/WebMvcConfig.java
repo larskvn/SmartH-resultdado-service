5083bcf6c663290a8e2427b50bf35a227cac6172
lars kvn <kvnyarbis@gmail.com>
2023-06-24 20:14:45 -0500

    update

--- src/main/java/com/smartheal/resultadoservice/WebConfiguration/WebMvcConfig.java
+++ src/main/java/com/smartheal/resultadoservice/WebConfiguration/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.smartheal.resultadoservice.WebConfiguration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
